26e7fe9111a0fed49b528e4aa0b0db32d7849e86
Etvendy Afrilio <etvendy@gmail.com>
2022-12-08 15:52:42 +0700

    Feat : Create API Images

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 
 //router 
 const categoriesRouter = require('./app/api/v1/categories/router');
+const imagesRouter = require('./app/api/v1/images/router');
 
 //middlewares
 const notFoundMiddleware = require('./app/middlewares/not-found');
@@ -27,6 +28,7 @@ app.get('/', (req,res) => {
 }); 
 
 app.use(v1, categoriesRouter);
+app.use(v1, imagesRouter);
 
 app.use(notFoundMiddleware);
 app.use(handleErrorMiddleware);
